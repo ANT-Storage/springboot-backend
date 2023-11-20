b6e8ba9dd95796d4b5438cecacbc7d7eaf6e7086
Albert Castiñeira Aranda <albertcastiaran@gmail.com>
2023-11-20 10:16:29 +0100

    initial commit

---
+++ src/main/java/com/ant_storage/ANT/Storage/AntStorageApplication.java
@@ -0,0 +1,13 @@
+package com.ant_storage.ANT.Storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ant_storage/ANT/Storage/AntStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ant_storage.ANT.Storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
