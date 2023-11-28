6b8b7d086ff09f644716897ca86d8610c6b94864
MounirEJH <ejhmounir@gmail.com>
2023-11-28 19:42:51 +0100

    New controller, servicontroller, service and repository of Tag class

--- src/main/java/com/ant_storage/ANT/Storage/controller/TagController.java
+++ src/main/java/com/ant_storage/ANT/Storage/controller/TagController.java
@@ -1,19 +1,14 @@
 package com.ant_storage.ANT.Storage.controller;
 
-import java.util.List;
-import java.util.Optional;
+import com.ant_storage.ANT.Storage.entity.Tag;
+import com.ant_storage.ANT.Storage.service.TagService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.ant_storage.ANT.Storage.entity.Tag;
-import com.ant_storage.ANT.Storage.service.TagService;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("antstorage/v1/tags")
@@ -31,14 +26,15 @@ public class TagController {
 		Optional<Tag> tag = tagService.getTagById(id);
 		return (tag.isPresent()) ? ResponseEntity.ok(tag) : ResponseEntity.notFound().build();
 	}
-
+	
 	@PostMapping
 	public Tag createTag(Tag tag) {
 		return tagService.saveTag(tag);
 	}
-
+	
 	@DeleteMapping("/{id}")
 	public void deleteTag(@PathVariable Integer id) {
 		tagService.deleteTag(id);
 	}
+	
 }
--- src/main/java/com/ant_storage/ANT/Storage/repository/TagRepository.java
+++ src/main/java/com/ant_storage/ANT/Storage/repository/TagRepository.java
@@ -3,7 +3,5 @@ package com.ant_storage.ANT.Storage.repository;
 import com.ant_storage.ANT.Storage.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface TagRepository extends JpaRepository<Tag, Integer>{
 }
